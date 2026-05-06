4af729c4792553feeb65f949a68ec22149de7121
Vidhinavadiya <vidhinavadiya90@gmail.com>
2026-05-06 14:11:40 +0530

    railway deployment setup

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,23 +3,28 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD || null,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME_DEVELOPMENT,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql'
   },
+
   test: {
     username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD || null,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME_TEST,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql'
   },
+
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME_PRODUCTION,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql'
   }
 };
