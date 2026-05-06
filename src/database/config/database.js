da598afc74bc08587e0e94c09d72be6a866388ec
Vidhinavadiya <vidhinavadiya90@gmail.com>
2026-05-06 15:09:02 +0530

    fix production response

--- src/database/config/database.js
+++ src/database/config/database.js
@@ -1,5 +1,7 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
 const dbName =
   process.env.NODE_ENV === 'production'
