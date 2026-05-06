0b9d54e1e65bd3b2bea5ab0dd8cff62d7645563f
Vidhinavadiya <vidhinavadiya90@gmail.com>
2026-05-06 17:28:18 +0530

    fix mysql env variables for railway

--- src/database/config/database.js
+++ src/database/config/database.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
+const seedAdmin = require('../seeders/20260506044342-Admin-login');
 
 const sequelize = new Sequelize(
     process.env.MYSQLDATABASE,
@@ -17,9 +18,24 @@ const connectDB = async () => {
     try {
         await sequelize.authenticate();
         console.log('✅ Database connected successfully!');
-        
+
         await sequelize.sync({ alter: true });
         console.log('✅ Tables synced!');
+        
+                // ✅ CHECK FIRST (IMPORTANT)
+        const [result] = await sequelize.query(
+            "SELECT * FROM users WHERE email = 'admin@healthcare.com'"
+        );
+
+        if (result.length === 0) {
+            await seedAdmin.up(
+                sequelize.getQueryInterface(),
+                Sequelize
+            );
+            console.log("✅ Admin Seeded");
+        } else {
+            console.log("ℹ️ Admin already exists");
+        }
     } catch (error) {
         console.error('❌ Unable to connect to database:', error);
     }
