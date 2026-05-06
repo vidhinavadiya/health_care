6cb557c9b27e64ca22e823de42702fa1c0bc09f5
Vidhinavadiya <vidhinavadiya90@gmail.com>
2026-05-06 14:50:40 +0530

    fix production response

--- src/controllers/admin/auth.controller.js
+++ src/controllers/admin/auth.controller.js
@@ -1,63 +1,24 @@
-// const { successResponse, serverError } = require('../../helpers/response');
-// const { AuthService } = require('../../services');
-
-// class AuthController {
-//     constructor () {
-//         this.authService = new AuthService();
-//     }
-//     //login admin
-//     login = async (req, res) => {
-//         try {
-//             const result = await this.authService.loginAdmin(req.body);
-//             return successResponse(res, {
-//                 success: true,
-//                 message: 'admin login successfully',
-//                 data: result
-//             });
-//         } catch (error) {
-//             return serverError(res, {
-//                 success: false,
-//                 message: error.message
-//             });
-//         }
-//     };
-// }
-
-// module.exports = AuthController;
-
 const { successResponse, serverError } = require('../../helpers/response');
 const { AuthService } = require('../../services');
 
 class AuthController {
-
     constructor () {
         this.authService = new AuthService();
     }
-
-    login = async (req, res) => {
+    //login admin
+    login = async (req) => {
         try {
-
             const result = await this.authService.loginAdmin(req.body);
-
-            return res.status(200).json(
-                successResponse(
-                    200,
-                    'admin login successfully',
-                    result
-                )
-            );
-
+            return successResponse({
+                success: true,
+                message: 'admin login successfully',
+                data: result
+            });
         } catch (error) {
-
-            return res.status(500).json(
-                serverError(
-                    500,
-                    error.message,
-                    null,
-                    error
-                )
-            );
-
+            return serverError({
+                success: false,
+                message: error.message
+            });
         }
     };
 }
